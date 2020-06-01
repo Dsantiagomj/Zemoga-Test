3bb616dbf9b2c20df7d266e01dd948765ee20d79
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2020-06-01 16:05:32 -0500

    project section two start point

--- src/pages/Auth.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Auth = () => {
-  return <div>Login / Signup</div>;
-};
-
-export default Auth;
--- src/services/Storage.js
+++
@@ -1,7 +0,0 @@
-export function setStorage(userData = {}) {
-  localStorage.setItem("userData", userData);
-}
-
-export function getStorage() {
-  return localStorage.getItem("userData");
-}
