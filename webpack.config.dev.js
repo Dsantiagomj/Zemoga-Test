6d39c120e41b86ea9c21e47da4196cc1b328f349
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2020-06-01 17:33:53 -0500

    add url-loader and setup images loaders for enviroments

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,4 +6,21 @@ module.exports = merge(baseConfig, {
   devServer: {
     port: 8000,
   },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[hash]-[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
 });
